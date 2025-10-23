8c1e3b63518561e8cf702ad8b166d5fb23e0c6bf
howie.peng <howie.peng@veeva.com>
2025-10-23 17:58:57 +0800

    init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is used by the application.
+    implementation(libs.guava)
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use Kotlin Test test framework
+            useKotlinTest("2.2.0")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "org.example.AppKt"
+}
